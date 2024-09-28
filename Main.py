2c9e3d2e1f0f357feaecdcb351ed7b2caf653ebf
balind1360 <141941014+balind1360@users.noreply.github.com>
2024-09-28 09:37:39 -0400

    COMMENTS

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ from vex import *\
 # Begin project code
 brain.screen.set_pen_width(100)
 
+# Disply screen
 brain.screen.print("                       Hello")
 brain.screen.next_row()
 brain.screen.print("                       I am ")
@@ -14,16 +15,22 @@ brain.screen.next_row()
 brain.screen.print("                   Cornyolanus Snow")
 
 controller_1.screen.print("    Welcome User")
-controller_1.rumble(".................-.")
+#controller_1.rumble(".................-.")
 
+# setting constants
 Intake.set_velocity(100,PERCENT)
+taking_in = False;
 
+# takes in the rings
 def take_in():
     Intake.spin(REVERSE)
     Track.spin(REVERSE)
 
-taking_in = False;
+# stops taking in the rings
+def motor_lock():
+    Intake.set_stopping(HOLD)
 
+# input loop
 while (True):
     if(taking_in):
         controller_1.buttonR2.pressed(take_in)
@@ -31,5 +38,4 @@ while (True):
         controller_1.buttonR2.pressed(motor_lock)
     
 
-def motor_lock():
-    Intake.set_stopping(HOLD)
+
